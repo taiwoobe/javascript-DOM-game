eb8b2142fced0661a3a7e16a5ab49969c587f2fb
Taiwo <taiwo.ibidapoobe@gmail.com>
2019-02-21 18:04:16 +0100

    Creating Event listeners & working with image DOM Manipulation

--- simple-js-DOM-game/app.js
+++ simple-js-DOM-game/app.js
@@ -9,20 +9,54 @@ GAME RULES:
 
 */
 
-var score, roundScore, activePlayer, dice;
+var score, roundScore, activePlayer;
 
 score = [0,0];
 roundScore = 0;
 activePlayer = 0;
 
-// Generating a random number for our dice. 
-dice = Math.floor(Math.random() * 6) + 1;
-console.log(dice);
-document.querySelector('#current-' + activePlayer).textContent = dice;
+
+// document.querySelector('#current-' + activePlayer).textContent = dice;
+
 
 // Reading values from the DOM using the querySelector() method.
 var scoreZero = document.querySelector('#score-0').textContent;
 console.log(scoreZero);
 
-// Modifying the CSS attribute of the DOM using the querySelector() method
+// Modifying the CSS attribute of the DOM using the querySelector() method. This can be set directly in CSS though. 
 document.querySelector('.dice').style.display = 'none';
+
+
+//
+document.getElementById('score-0').textContent = '0'
+document.getElementById('score-1').textContent = '0'
+document.getElementById('current-0').textContent = '0'
+document.getElementById('current-1').textContent = '0'
+
+// Function that is called once the roll button is cliked.
+// function btnRollClicked() {
+//    Do something here
+// }
+
+// Getting the button that performs the roll dice function.
+
+// The below code shows how to use a callback function in the addEventListener method. 
+// The callback function is basically a function that we dont call ourselves but allow another function to call it for us.
+
+// document.querySelector('.btn-roll').addEventListener('click', btnRollClicked);
+
+// We are making use of an anonymous function in the below code. 
+//An anonymous function is a function that doesnt have a name and cannot be reused outside of its scope. 
+
+document.querySelector('.btn-roll').addEventListener('click', function() {
+    // Generating a random number for our dice. 
+    var dice = Math.floor(Math.random() * 6) + 1;
+    console.log(dice);
+
+    // Display the result
+    var diceDOM = document.querySelector('.dice');
+    diceDOM.style.display = 'block';
+    diceDOM.src = 'dice-' + dice + '.png';
+
+    // Update the round score iff dice rolled is not 1. 
+});
